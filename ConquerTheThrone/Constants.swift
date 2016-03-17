d632aa990a79f1a5a31c335ac2c7a1b3726ef1e2
lolivier04 <lsecrease@gmail.com>
2016-03-16 23:43:30 -0500

    Added Firbase Data & Constants

---
+++ ConquerTheThrone/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  ConquerTheThrone
+//
+//  Created by lsecrease on 3/16/16.
+//  Copyright © 2016 lsecrease. All rights reserved.
+//
+
+import Foundation
---
+++ ConquerTheThrone/DataService.swift
@@ -0,0 +1,9 @@
+//
+//  DataService.swift
+//  ConquerTheThrone
+//
+//  Created by lsecrease on 3/16/16.
+//  Copyright © 2016 lsecrease. All rights reserved.
+//
+
+import Foundation
